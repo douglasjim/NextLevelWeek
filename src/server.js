91acca0cc2442b95e7ca217dddf18cf3f49c03e4
Douglasjinn <douglasjin1@gmail.com>
2020-06-07 17:56:12 -0300

    Subindo o SQL

--- src/server.js
+++ src/server.js
@@ -8,14 +8,39 @@ server.use(express.static("Public")) //com essa linha de codigo conseguimos pega
 //a palavra public quer dizer que esta pegando todos os arquivos e mostrando pra todos os arquivos que necessitam um do outro
 
 
+
+
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true //
+})
+
+
+
 //get pedir algo neste caso esta pedindo a barra /
 //pagina inicial
 //req:pergunta = requisicao = pedido
 //res: resposta
 //__dirname = diretorio
 server.get("/",function(req, res){
-    res.sendFile(__dirname + "/views/index.html") //aqui seria a resposta que o servidor faz
+    return res.render("index.html",{ title: "Um titulo" }) //aqui seria a resposta que o servidor faz
 })
 
+
+//segunda rota
+server.get("/create-point",function(req, res){
+    return res.render("create-point.html") //aqui seria a resposta que o servidor faz
+})
+
+
+server.get("/search",function(req, res){
+    return res.render("search-results.html") //aqui seria a resposta que o servidor faz
+})
 //ligar o servidor 
 server.listen(3000) //aqui estamos marcado qual porta irá abrir o servidor
+
+// server.get("/create-point",function(req, res){
+//     res.sendFile(__dirname + "/views/create-point.html") //aqui seria a resposta que o servidor faz
+// })
